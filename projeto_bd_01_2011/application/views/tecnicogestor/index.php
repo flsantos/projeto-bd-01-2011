23374d6b89837d3c0f6a5f543c44115130011952
loamilucas@gmail.com <loamilucas@gmail.com@4ac118ab-f360-582f-9f85-cd2c15abafd1>
2011-07-04 06:02:06 +0000

    View de técnico/gestor alterada com o novo layout

--- projeto_bd_01_2011/application/views/tecnicogestor/index.php
+++ projeto_bd_01_2011/application/views/tecnicogestor/index.php
@@ -1,45 +1,83 @@
-<?php doctype() ?>
+<?=doctype('html5') ?>
 
 <html>
 <head>
 
-<?php meta() ?>
-
+	<?=meta('Content-type', 'text/html; charset=utf-8', 'equiv');?> 
+	<link rel="stylesheet" href="<?=base_url()?>public/css/corpo/default.css" type="text/css" />
+	<link rel="stylesheet" href="<?=base_url()?>public/css/tablecloth/tablecloth.css" type="text/css" />
+		
+	<script type="text/javascript" src="<?=base_url()?>public/css/tablecloth/tablecloth.js"> </script>
 </head>
 
 <body>
-	<h1> Técnicos/Gestores: </h1>
-	
-	<div>
-		<table>
-			<thead>
-				<tr>
-					<th>Nome</th>
-					<th>Cargo</th>
-				</tr>
-			</thead>
-			<tbody>
-			<?php if (count($tecnicogestores) <= 0): ?>
-					<tr>
-						<td  colspan="3">Não existem dados cadastrados</td>
-					</tr>
-			<?php else: ?>
-				<?php foreach ($tecnicogestores as $tecnicogestor): ?>
-					<tr>
-						<td><?=$tecnicogestor->nome?></td>
-						<td><?=$tecnicogestor->perfil != null ? ($tecnicogestor->perfil == 1 ? 'Técnico' : ($tecnicogestor->perfil == 2 ? 'Gestor' : '')) : '' ?></td>
-						<td><?=anchor('tecnicogestor/visualizar/'.$tecnicogestor->id, 'Visualizar', 'Visualizar registro')?></td>
-						<td><?=anchor('tecnicogestor/alterar/'.$tecnicogestor->id, 'Editar', 'Editar registro')?></td>
-						<td><?=anchor('tecnicogestor/excluir/'.$tecnicogestor->id, 'Excluir', 'Excluir registro')?></td>
-					</tr>	
-				<?php endforeach;?>
-			<?php endif; ?>
-			</tbody>
-		</table>
-		<br/>
-		<?=anchor('tecnicogestor/alterar', 'Cadastrar novo técnico/gestor', 'Novo registro')?>
+
+<!-- start header -->
+<div id="logo">
+	<h1><?=anchor('#', 'Listagem', '')?></h1>
+	<h2> &raquo;&nbsp;&nbsp;&nbsp;TÃ©cnico/Gestor</h2>
+</div>
+<div id="header">
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><?=anchor('tecnicogestor/alterar', 'Cadastrar novo tÃ©cnico/gestor', 'Novo registro')?></li>
+		</ul>
 	</div>
+</div>
+<!-- end header -->
 
 
+<div id="wrapper">
+<!-- start page -->
+<div id="page">
+	<!-- start content -->
+	<div id="content">
+		<div class="post">
+			<h1 class="title"></h1>
+			<div class="entry">
+            	<table>
+					<thead>
+						<tr>
+							<th>Nome</th>
+							<th>Cargo</th>
+							<th></th>
+							<th></th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php if (count($tecnicogestores) <= 0): ?>
+							<tr>
+								<td  colspan="3">NÃ£o existem dados cadastrados</td>
+							</tr>
+					<?php else: ?>
+						<?php foreach ($tecnicogestores as $tecnicogestor): ?>
+							<tr>
+								<td><?=$tecnicogestor->nome?></td>
+								<td><?=$tecnicogestor->perfil != null ? ($tecnicogestor->perfil == 1 ? 'TÃ©cnico' : ($tecnicogestor->perfil == 2 ? 'Gestor' : '')) : '' ?></td>
+								<td><?=anchor('tecnicogestor/visualizar/'.$tecnicogestor->id, 'Visualizar', 'Visualizar registro')?></td>
+								<td><?=anchor('tecnicogestor/alterar/'.$tecnicogestor->id, 'Editar', 'Editar registro')?></td>
+								<td><?=anchor('tecnicogestor/excluir/'.$tecnicogestor->id, 'Excluir', 'Excluir registro')?></td>
+							</tr>	
+						<?php endforeach;?>
+					<?php endif; ?>
+					</tbody>
+				</table>
+				
+			</div>
+		</div>
+	</div>
+	<!-- end content -->
+	<div style="clear: both;">&nbsp;</div>
+</div>
+</div>
+<!-- end page -->
+<!-- start footer -->
+<div id="footer">
+	<p id="legal">( c ) 2008. All Rights Reserved. <a href="http://www.freecsstemplates.org/">Bestfriends</a> designed by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>.</p>
+</div>
+<!-- end footer -->
 </body>
 </html>
+
+			
