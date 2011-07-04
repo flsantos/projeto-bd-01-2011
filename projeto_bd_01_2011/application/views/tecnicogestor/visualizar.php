aa07177bd39f22987de67f423fa3ae7b8b46d393
loamilucas@gmail.com <loamilucas@gmail.com@4ac118ab-f360-582f-9f85-cd2c15abafd1>
2011-07-04 20:19:47 +0000

    View visualizar com o template de layout.

--- projeto_bd_01_2011/application/views/tecnicogestor/visualizar.php
+++ projeto_bd_01_2011/application/views/tecnicogestor/visualizar.php
@@ -1,119 +1,152 @@
 <?php $this->load->helper('form'); ?>
-<?= doctype() ?>
+<?=doctype('html5') ?>
 <html>
 <head>
-	<?= meta() ?>
+	<?=meta('Content-type', 'text/html; charset=utf-8', 'equiv');?>
+	<title>Viualizar Dados de um TÃ©cnico/Gestor</title> 
+	<link rel="stylesheet" href="<?=base_url()?>public/css/corpo/default.css" type="text/css" />
+	
 </head>
 
 <body>
-	
-	<h1>Visualizar Técnico/Gestor</h1>
-	
-		<div>
-			<?=form_label('Nome: ', 'nome')?>
-			<?=$pessoas[0]->nome?>
-		</div>
-		<div>
-			<?=form_label('Estado Civil: ', 'estado_civil')?> 
-			<?php
-				if ($pessoas[0]->estado_civil == 1) {
-					echo $pessoas[0]->sexo == 1 ? "Solteiro" : "Solteira";
-				} elseif ($pessoas[0]->estado_civil == 2) {
-					echo $pessoas[0]->sexo == 1 ? "Casado" : "Casada";
-				} elseif ($pessoas[0]->estado_civil == 3) {
-					echo $pessoas[0]->sexo == 1 ? "Divorciado" : "Divorciada";
-				} elseif ($pessoas[0]->estado_civil == 4) {
-					echo $pessoas[0]->sexo == 1 ? "Viúvo" : "Viúva";
-				}
-			?>
-		</div>
-		<div>
-			<?=form_label('Sexo: ', 'sexo')?>
-			<?php
-				if ($pessoas[0]->sexo == 1) {
-					echo "Masculino";
-				} elseif ($pessoas[0]->sexo == 2) {
-					echo "Feminino";
-				}
-			?>
-		</div>
-		<div>
-			<?=form_label('Cargo: ', 'tipo')?>
-			<?php
-				if ($tecnicogestor[0]->perfil == 1) {
-					echo "Técnico";
-				} elseif ($tecnicogestor[0]->perfil == 2) {
-					echo "Gestor";
-				}
-			?>
-		</div>
-		<div>
-			<?=form_label('Site: ', 'site')?>
-			<?=$tecnicogestor[0]->site?>
-		</div>
-		<div>
-			<?=form_label('Blog: ', 'blog')?>
-			<?=$tecnicogestor[0]->blog?>
-		</div>
-		<div>
-			<?=form_label('Twitter: ', 'twitter')?>
-			<?=$tecnicogestor[0]->twitter?>
-		</div>
-		<div id="emails">
-			<?php $i = 1;?>
-			<?php foreach ($emails as $email):?>
-				<div id="email<?php echo $i?>">
-					Email <?=$i?>: 
-					<?=$email->email?>
-				</div>
-				<?php $i++;?>
-			<?php endforeach;?>
-		</div>
-		<div id="telefones">
-			<?php $i = 1;?>
-			<?php foreach ($telefones as $telefone):?>
-				<div id="telefone<?php echo $i?>">
-					Telefone <?=$i?> <?=($telefone->tipo == 1 ? '(Fixo)' : '(Celular)')?>: 
-					<?=$telefone->telefone?>
-				</div>
-				<?php $i++;?>
-			<?php endforeach;?>
-		</div>
-		<div>
-			<?=form_label('Endereço Institucional: ', 'end_institucional')?>
-			<?=$tecnicogestor[0]->end_institucional ?>
-		</div>
-		<div>
-			<?=form_label('CV Lates (Link): ', 'cv_lates')?>
-			<?=$tecnicogestor[0]->cv_lattes ?>
-		</div>
-		<div id="titulacoes">
-			<?php $i = 1;?>
-			<?php foreach ($titulacoes as $titulacao):?>
-				<div id="titulacao<?php echo $i?>">
-					Titulacao <?=$i?>:
-					<?=$titulacao->data?> - <?=$titulacao->nome?>
-				</div>
-				<?php $i++;?>
-			<?php endforeach;?>
-		</div>
-		<div>
-			<?=form_label('Graduação: ', 'graduacao')?>
-			<?=$tecnicogestor[0]->graduacao?>
-		</div>
-		<div>
-			<?=form_label('Data de Admissão: ', 'data_admissao')?>
-			<?=$tecnicogestor[0]->data_admissao?>
-		</div>
-		<div>
-			<?=form_label('Data de Saída: ', 'data_saida')?>
-			<?=$tecnicogestor[0]->data_saida != null ? $tecnicogestor[0]->data_saida : '----------'?>
-		</div>
-		<br/>
-	
-	
-	
 
+<!-- start header -->
+<div id="logo">
+	<h1><?=anchor('#', 'Visualizar TÃ©cnico/Gestor', '')?></h1>
+	<h2> &raquo;&nbsp;&nbsp;&nbsp;TÃ©cnico/Gestor</h2>
+</div>
+<div id="header">
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><?=anchor('tecnicogestor/alterar', 'Cadastrar novo tÃ©cnico/gestor', 'Novo registro')?></li>
+		</ul>
+	</div>
+</div>
+<!-- end header -->
 
+<div id="wrapper">
+<!-- start page -->
+<div id="page">
+	<!-- start content -->
+	<div id="content">
+		<div class="post">
+			<h1 class="title"></h1>
+			<div class="entry">
+	
+				<div>
+					<?=form_label('Nome: ', 'nome')?>
+					<?=$pessoas[0]->nome?>
+				</div>
+				<div>
+					<?=form_label('Estado Civil: ', 'estado_civil')?> 
+					<?php
+						if ($pessoas[0]->estado_civil == 1) {
+							echo $pessoas[0]->sexo == 1 ? "Solteiro" : "Solteira";
+						} elseif ($pessoas[0]->estado_civil == 2) {
+							echo $pessoas[0]->sexo == 1 ? "Casado" : "Casada";
+						} elseif ($pessoas[0]->estado_civil == 3) {
+							echo $pessoas[0]->sexo == 1 ? "Divorciado" : "Divorciada";
+						} elseif ($pessoas[0]->estado_civil == 4) {
+							echo $pessoas[0]->sexo == 1 ? "ViÃºvo" : "ViÃºva";
+						}
+					?>
+				</div>
+				<div>
+					<?=form_label('Sexo: ', 'sexo')?>
+					<?php
+						if ($pessoas[0]->sexo == 1) {
+							echo "Masculino";
+						} elseif ($pessoas[0]->sexo == 2) {
+							echo "Feminino";
+						}
+					?>
+				</div>
+				<div>
+					<?=form_label('Cargo: ', 'tipo')?>
+					<?php
+						if ($tecnicogestor[0]->perfil == 1) {
+							echo "TÃ©cnico";
+						} elseif ($tecnicogestor[0]->perfil == 2) {
+							echo "Gestor";
+						}
+					?>
+				</div>
+				<div>
+					<?=form_label('Site: ', 'site')?>
+					<?=$tecnicogestor[0]->site?>
+				</div>
+				<div>
+					<?=form_label('Blog: ', 'blog')?>
+					<?=$tecnicogestor[0]->blog?>
+				</div>
+				<div>
+					<?=form_label('Twitter: ', 'twitter')?>
+					<?=$tecnicogestor[0]->twitter?>
+				</div>
+				<div id="emails">
+					<?php $i = 1;?>
+					<?php foreach ($emails as $email):?>
+						<div id="email<?php echo $i?>">
+							Email <?=$i?>: 
+							<?=$email->email?>
+						</div>
+						<?php $i++;?>
+					<?php endforeach;?>
+				</div>
+				<div id="telefones">
+					<?php $i = 1;?>
+					<?php foreach ($telefones as $telefone):?>
+						<div id="telefone<?php echo $i?>">
+							Telefone <?=$i?> <?=($telefone->tipo == 1 ? '(Fixo)' : '(Celular)')?>: 
+							<?=$telefone->telefone?>
+						</div>
+						<?php $i++;?>
+					<?php endforeach;?>
+				</div>
+				<div>
+					<?=form_label('EndereÃ§o Institucional: ', 'end_institucional')?>
+					<?=$tecnicogestor[0]->end_institucional ?>
+				</div>
+				<div>
+					<?=form_label('CV Lates (Link): ', 'cv_lates')?>
+					<?=$tecnicogestor[0]->cv_lattes ?>
+				</div>
+				<div id="titulacoes">
+					<?php $i = 1;?>
+					<?php foreach ($titulacoes as $titulacao):?>
+						<div id="titulacao<?php echo $i?>">
+							Titulacao <?=$i?>:
+							<?=$titulacao->data?> - <?=$titulacao->nome?>
+						</div>
+						<?php $i++;?>
+					<?php endforeach;?>
+				</div>
+				<div>
+					<?=form_label('GraduaÃ§Ã£o: ', 'graduacao')?>
+					<?=$tecnicogestor[0]->graduacao?>
+				</div>
+				<div>
+					<?=form_label('Data de AdmissÃ£o: ', 'data_admissao')?>
+					<?=$tecnicogestor[0]->data_admissao?>
+				</div>
+				<div>
+					<?=form_label('Data de SaÃ­da: ', 'data_saida')?>
+					<?=$tecnicogestor[0]->data_saida != null ? $tecnicogestor[0]->data_saida : '----------'?>
+				</div>
+		</div>
+		</div>
+	</div>
+	<!-- end content -->
+	<div style="clear: both;">&nbsp;</div>
+</div>
+</div>
+<!-- end page -->
+<!-- start footer -->
+<div id="footer">
+	<p id="legal">( c ) 2008. All Rights Reserved. <a href="http://www.freecsstemplates.org/">Bestfriends</a> designed by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>.</p>
+</div>
+<!-- end footer -->
+	
+	
 </body>
 </html>
